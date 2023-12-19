a280e0a0bc6194a0feb95341762372abee08b4f3
jaemin <lemon__96@naver.com>
2023-12-19 17:10:53 +0900

    chore: update test code - app

--- packages/study_project/src/App.tsx
+++ packages/study_project/src/App.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      <span>hello</span>
+    </div>
+  );
 }
 
 export default App;
