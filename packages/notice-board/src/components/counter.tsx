00e3045646252d0cb4988cc07e516845a3a030f8
jaemin96 <lemon__96@naver.com>
2024-02-06 17:59:05 +0900

    feat: counter 컴포넌트 selector 구현

--- packages/notice-board/src/components/counter.tsx
+++ packages/notice-board/src/components/counter.tsx
@@ -1,19 +1,23 @@
-import { useEffect } from 'react';
-import { atom, useRecoilState, useRecoilValue } from 'recoil';
+import { atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 
 const CounterState = atom({
   key: 'countState',
   default: 0,
 });
 
+const DoubleCount = selector({
+  key: 'doubleCount',
+  get: ({ get }) => {
+    const count = get(CounterState);
+
+    return count * 2;
+  },
+});
+
 const Counter = () => {
   const [count, setCount] = useRecoilState(CounterState);
 
-  const countValue = useRecoilValue(CounterState);
-
-  useEffect(() => {
-    console.log({ countValue });
-  }, [countValue]);
+  const doubleCount = useRecoilValue(DoubleCount);
 
   const increase = () => {
     setCount((prev) => prev + 1);
@@ -26,6 +30,7 @@ const Counter = () => {
     <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
       <h1>Counter</h1>
       <span>{`current value -- ${count}`}</span>
+      <span>{`value * 2 -- ${doubleCount}`}</span>
       <div style={{ display: 'flex', gap: '1rem' }}>
         <button onClick={increase}>+</button>
         <button onClick={decrease}>-</button>
