ea1c8fefeefceaba14431b50b69ed5e221444c43
jaemin96 <lemon__96@naver.com>
2024-02-20 17:29:42 +0900

    fix: App 컴포넌트 텍스트 추가

--- packages/notice-board/src/App.tsx
+++ packages/notice-board/src/App.tsx
@@ -4,7 +4,7 @@ import Counter from './components/counter';
 const App = () => {
   return (
     <>
-      <h1>hello</h1>
+      <h1>hello, world</h1>
       <RecoilRoot>
         <Counter />
       </RecoilRoot>
